2072b153829518d31ce46f98f3f032d50de69c5c
gojutin <gojutin@gmail.com>
2019-09-17 15:49:05 -0400

    Fixed issue with testing components with `useStaticQuery`

--- src/components/seo/seo.test.tsx
+++ src/components/seo/seo.test.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import SEO from "./seo";
 import { render, cleanup } from "@testing-library/react";
-import { useStaticQuery } from "../../../__mocks__/gatsby";
+import * as Gatsby from "gatsby";
+const useStaticQuery = jest.spyOn(Gatsby, "useStaticQuery");
 
 beforeEach(() => {
-  useStaticQuery.mockImplementationOnce(({ render }) =>
-    render({
-      data: {
-        site: {
-          siteMetadata: {
-            title: "Test",
-            description: "Test",
-            author: "@test",
-          },
+  useStaticQuery.mockImplementationOnce(() => {
+    return {
+      site: {
+        siteMetadata: {
+          title: "Test",
+          description: "Test",
+          author: "@test",
         },
       },
-    }),
-  );
+    };
+  });
 });
 
 afterEach(cleanup);
