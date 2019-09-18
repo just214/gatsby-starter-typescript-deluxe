6b31b7983f67f62c01bab8cf4def999e11120287
gojutin <gojutin@gmail.com>
2019-09-17 22:21:44 -0400

    Updated dependencies.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,5 @@
 //***********************************************************
 module.exports = {
   extends: ["gojutin"],
-  rules: {
-    // "@typescript-eslint/explicit-function-return-type": 0,
-  },
+  rules: {},
 };
